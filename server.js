2e898f31dfa0b66b453aec60bff4c0332e8d29a9
unknown <syyadkhan@yahoo.com>
2019-01-11 18:29:14 -0500

    mongo connected

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Mongo Connected"))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
