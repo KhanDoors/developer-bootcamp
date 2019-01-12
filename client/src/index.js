8009bc9b9e9cb49eb552d798aab479089723e9cf
unknown <syyadkhan@yahoo.com>
2019-01-12 12:59:48 -0500

    authentication and signup form complete

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import Signup from "./components/Auth/Signup";
+import Signin from "./components/Auth/Signin";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 });
 
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+);
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Root />
   </ApolloProvider>,
   document.getElementById("root")
 );
--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,11 @@
+const jwt = require("jsonwebtoken");
+const secret = require("./config/secret");
+
+const createToken = (user, secret, expiresIn) => {
+  const { username, email } = user;
+  return jwt.sign({ username, email }, secret, { expiresIn });
+};
+
 exports.resolvers = {
   Query: {
     getAllRecipes: async (root, args, { Recipe }) => {
@@ -19,6 +27,19 @@ exports.resolvers = {
         username
       }).save();
       return newRecipe;
+    },
+
+    signupUser: async (root, { username, email, password }, { User }) => {
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new Error("User already exists");
+      }
+      const newUser = await new User({
+        username,
+        email,
+        password
+      }).save();
+      return { token: createToken(newUser, secret.secretOrKey, "1hr") };
     }
   }
 };
